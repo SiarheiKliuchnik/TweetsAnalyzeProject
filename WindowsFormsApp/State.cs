4db1ba9e83b56c30e454d20f4cf2e66a654076d8
necr0me <ortusprogressor@gmail.com>
2021-03-08 12:47:01 +0300

    Create State.cs
    Added a state entity

---
+++ WindowsFormsApp/State.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp
+{
+    public class State
+    {
+        public List<Tweet> Tweets
+        { get; set; }
+        public float Weigth
+        { get; set; }
+        public List<List<List<Coordinates>>> Polygons;
+        { get; set; }
+    }
+}
