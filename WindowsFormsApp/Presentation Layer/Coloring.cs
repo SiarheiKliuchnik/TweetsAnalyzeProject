4c6ae63f2b74b76ea4480e40141dddd150265653
necr0me <ortusprogressor@gmail.com>
2021-03-28 12:30:38 +0300

    Changed method of computing weight color

--- WindowsFormsApp/Presentation Layer/Coloring.cs
+++ WindowsFormsApp/Presentation Layer/Coloring.cs
@@ -12,7 +12,6 @@ namespace WindowsFormsApp.Presentation_Layer
         public static Color SetColors(float value)
         {
             int green = 255, red = 255, blue = 255;
-            float coef = 0;
             if (float.IsNaN(value))
                 return Color.Gray;
             if (value > 0)
@@ -21,15 +20,13 @@ namespace WindowsFormsApp.Presentation_Layer
                 {
                     red = 255;
                     green = 255;
-                    coef = 0.5F / 255F;
-                    blue = 255 - Convert.ToInt32(value / coef);
+                    blue = Convert.ToInt32(255 - 255*value*2);
                 }
-                else if (value > 0.5)
+                else if (value >= 0.5)
                 {
                     red = 255;
                     blue = 0;
-                    coef = 0.5F / 128F;
-                    green = 255 - Convert.ToInt32(value / coef);
+                    green = Convert.ToInt32(255-255 *(value-0.5f));
                 }
             }
             else if (value < 0)
@@ -38,19 +35,17 @@ namespace WindowsFormsApp.Presentation_Layer
                 {
                     blue = 255;
                     green = 255;
-                    coef = 0.5F / 255F;
-                    red = 255 - Math.Abs(Convert.ToInt32(value / coef));
+                    red = Convert.ToInt32(255 - 255*Math.Abs(value*2));
 
                 }
-                else if (value < -0.5)
+                else if (value <= -0.5)
                 {
                     blue = 255;
                     red = 0;
-                    coef = 0.5F / 128F;
-                    green = 255 - Math.Abs(Convert.ToInt32(value / coef));
+                    green = Convert.ToInt32(255 -255 * Math.Abs((value+0.5f)));
                 }
             }
-            return Color.FromArgb(IsOutOfByte(red), IsOutOfByte(green), IsOutOfByte(blue));
+                return Color.FromArgb(IsOutOfByte(red), IsOutOfByte(green), IsOutOfByte(blue));
         }
         private static int IsOutOfByte(int value)
         {
