595109374976a1fdc5844db9dc767615851c0fa1
SiarheiKliuchmik <sergejtvnf@gmail.com>
2021-04-02 00:29:28 +0300

    nothing

--- WindowsFormsApp/Presentation Layer/Form1.cs
+++ WindowsFormsApp/Presentation Layer/Form1.cs
@@ -66,7 +66,7 @@ namespace WindowsFormsApp
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-
+           
         }
 
         private void gMapControl1_Load(object sender, EventArgs e)
